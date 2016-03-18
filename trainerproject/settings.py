fdacf6fa9e4c38bb1d409d5af35a3c0f7e0a6725
Christopher Ogle <thecogle@gmail.com>
2016-03-18 18:02:46 -0500

    Merged

--- trainerproject/settings.py
+++ trainerproject/settings.py
@@ -38,7 +38,7 @@ REST_FRAMEWORK = {
         'rest_framework.renderers.JSONRenderer',
         'rest_framework.renderers.BrowsableAPIRenderer',
     ),
-    
+
 }
 
 MIDDLEWARE_CLASSES = [
@@ -76,13 +76,25 @@ WSGI_APPLICATION = 'trainerproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
+'''
 DATABASES = {
       'default': {
           'ENGINE': 'django.db.backends.sqlite3',
           'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
       }
   }
+'''
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ.get('DATABASE_NAME'),
+        'USER': os.environ.get('DATABASE_USER_NAME'),
+        'PASSWORD': os.environ.get('DATABASE_USER_PASSWORD'),
+        'HOST': 'localhost',
+        'PORT': '5431',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/1.9/ref/settings/#auth-password-validators
