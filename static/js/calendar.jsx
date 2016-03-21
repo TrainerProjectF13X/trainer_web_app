f4888e841aee45991a04635c25bedc3ae392807d
JooHee Lee <jooheelee@jl-mbp.local>
2016-03-20 22:03:25 -0500

    added static calendar

--- static/js/calendar.jsx
+++ static/js/calendar.jsx
@@ -1,15 +1,23 @@
 var React = require('react')
 var ReactDOM = require('react-dom')
+var _ = require('underscore');
+var clndr = require('clndr')
+var moment = require('moment')
 
 export default class Calendar extends React.Component {
     constructor(props) {    /* Note props is passed into the constructor in order to be used */
       super(props);
 
     }
-
+    componentDidMount(){
+        $('.parent-element').clndr();
+    }
     render(){
         return (
+            <div>
             <h1>Calendar</h1>
+            <div className="parent-element"></div>
+            </div>
         );
     }
 }
