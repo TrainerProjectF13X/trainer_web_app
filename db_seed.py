c0699f1a665d5d6dfce8f4ac723a8c2e37004ac0
Christopher Ogle <thecogle@gmail.com>
2016-03-25 23:46:05 -0500

    Fixed minor bug

--- db_seed.py
+++ db_seed.py
@@ -25,20 +25,23 @@ class SeedDatabase(object):
     CONST_BASE_FIRSTNAME =  "test_account_"
     CONST_BASE_USER_EMAIL = "null@null_"
 
-    def __init__(self, debug, verbose):
-        self.debug = debug
-        self.verbose = verbose
+    def __init__(self, args):
+        self.debug = args.debug
+        self.verbose = args.verbose
+        self.assign = args.assign_only
 
     def main(self):
         user_list = []
         num_user_accounts = int(input("How many user accounts do you want to create in total? "))
+        if num_user_accounts == 0: return
         self.create_user(num_user_accounts, user_list)
         num_trainer_accounts = num_user_accounts + 1
         while not num_trainer_accounts <= num_user_accounts:
             num_trainer_accounts = int(input("How many trainer accounts do you want to create in total? "))
 
-        self.create_trainer(num_trainer_accounts, user_list)
+        user_list = self.create_trainer(num_trainer_accounts, user_list)
         self.create_reglr_user(user_list)
+        self.assign_trainer_user()
 
     def create_user(self, num, ul):
         all_users = User.objects.all()
@@ -63,8 +66,13 @@ class SeedDatabase(object):
         for i in trainer_list:
             trainer = TrainerAccount.objects.create(user=ul[i],auth_token=self.create_token(ul[i]))
             trainer.save()
-        for i in trainer_list:
-            ul.remove(ul[i])
+        ret = []
+        count = 0
+        for ele in ul:
+            if count not in trainer_list:
+                ret.append(ele)
+            count  = count + 1
+        return ret
 
     def create_reglr_user(self, ul):
         for user in ul:
@@ -81,13 +89,35 @@ class SeedDatabase(object):
                 break;
         return hex_dig
 
+    def assign_trainer_user(self):
+        assign = str(input("Would you like to assign a trainer a client\s? ")).lower()
+        while "yes" in assign or "y" in assign:
+            randomly = str(input("Would you like the program to randomly assign to first trainer IN DATABASE? (Yes or No) ")).lower()
+            if "yes" in randomly or "y" in randomly:
+                if not TrainerAccount.objects.all():
+                    print("No trainers currently exist in the Database please create some and then rerun with assign option")
+                    return
+                else:
+                    trainer = TrainerAccount.objects.all()[0]
+                    free_users = RegularAccount.objects.filter(trainer=None)
+                    if not free_users:
+                        print("There are currently no free users please create some then rerun")
+                        return
+                    str(input("Out of {} unassigned accounts how many would you like to assign? ").format(len(free_users)))
+            else:
+                print("#TODO#")
+            assign = str(input("Would you like to assign a trainer a client\s? ")).lower()
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
 
-    parser.add_argument('-d','--debug', action='store_true', default=False)
-    parser.add_argument('-v','--verbose', action='store_true', default=False)
+    parser.add_argument('-a','--assign_only', action='store_true', default=False)
+    parser.add_argument('-d','--debug',       action='store_true', default=False)
+    parser.add_argument('-v','--verbose',     action='store_true', default=False)
 
     args = parser.parse_args()
-
-    run_instance = SeedDatabase(args.debug, args.verbose)
-    run_instance.main()
+    run_instance = SeedDatabase(args)
+    if run_instance.assign:
+        run_instance.assign_trainer_user()
+    else:
+        run_instance.main()
