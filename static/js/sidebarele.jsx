adf624b92380ea4acfc37b8da52d03b03d2e864e
JooHee Lee <jooheelee@wustl.edu>
2016-03-27 13:06:31 -0500

    sidebar icons work now

--- static/js/sidebarele.jsx
+++ static/js/sidebarele.jsx
@@ -5,10 +5,24 @@ export default class SideBarEle extends React.Component {
     constructor(props) {    /* Note props is passed into the constructor in order to be used */
       super(props);
 
+
     }
+
     render(){
+        var iconArray = {
+            "My Profile"        : <i className="material-icons left">face</i>,
+            "Calendar"          : <i className="material-icons left">event</i>,
+            "Saved Workouts"    : <i className="material-icons left">storage</i>,
+            "Progress"          : <i className="material-icons left">equalizer</i>,
+            "My Trainees"       : <i className="material-icons left">contacts</i>,
+            "Trainees Nearby"   : <i className="material-icons left">near_me</i>,
+            "Trainers Nearby"   : <i className="material-icons left">near_me</i>,
+            "Settings"          : <i className="material-icons left">settings</i>
+        };
+
         let eleClass = "";
         let eleIcon = "";
+        let icon = iconArray[this.props.text];
 
         if(this.props.eleIndex === this.props.curElement){
             eleClass = "active bold";
@@ -18,7 +32,7 @@ export default class SideBarEle extends React.Component {
         }
 
         return (
-            <li className={eleClass}><a onClick={this.props.onClick}>{this.props.text}</a></li>
+            <li className={eleClass}><a onClick={this.props.onClick}>{icon}{this.props.text}</a></li>
         );
     }
 }
